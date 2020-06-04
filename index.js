c8dce40acd19270a90b64e487e2c2e208161cd50
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2020-06-04 13:08:28 +0530

    fingers crossed

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use('/',require('./Router/Router'));
 
 if(process.env.NODE_ENV==="production"){
     app.use(Express.static('./client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
 }
 
 
